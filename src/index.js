9cc41908672d26e0b51f3bc724363672f0715885
[your <alessandrofigo24@gmail.com>
2021-08-18 14:16:17 +0700

    new file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+import "./styles/reset.css";
+import "./styles/theme.css";
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
